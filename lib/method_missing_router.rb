f74d55c733718b1affdc443cdade9040518d95fa
Matt Margolis <matt@mattmargolis.net>
2011-11-20 16:12:59 -0600

    Specs for instance and class routing of methods

--- lib/method_missing_router.rb
+++ lib/method_missing_router.rb
@@ -2,14 +2,16 @@ require "method_missing_router/version"
 module MethodMissingRouter
   def self.included(base)
     base.extend(ClassMethods)
+    base.reset_method_missing_routes!
     base.instance_variable_set("@class_method_missing_routes", [])
     base.instance_variable_set("@method_missing_routes", [])
   end
 
   def method_missing(meth, *args, &block)
     self.class.instance_variable_get("@method_missing_routes").each do |matcher, target|
-      self.send(target, *args, &block) if matcher =~ meth
+      return self.send(target, *args, &block) if matcher =~ meth
     end
+    super(meth, *args, &block)
   end
 
   module ClassMethods
@@ -21,10 +23,18 @@ module MethodMissingRouter
       @method_missing_routes << [matcher, target]
     end
 
+    def reset_method_missing_routes!
+      @method_missing_routes = []
+      @class_method_missing_routes = []
+    end
+
     def method_missing(meth, *args, &block)
       @class_method_missing_routes.each do |matcher, target|
-        self.send(target, *args, &block) if matcher =~ meth
+        if matcher =~ meth
+          return self.send(target, *args, &block)
+        end
       end
+      super(meth, *args, &block)
     end
   end
 end
--- spec/lib/method_missing_router_spec.rb
+++ spec/lib/method_missing_router_spec.rb
@@ -1,32 +1,64 @@
 require 'spec_helper'
 
-describe MethodMissingRouter do
-  context 'class method routing' do
-    example 'route any missing message to the same target' do
-      class TestMissingMethodRouter
-        include MethodMissingRouter
-        class_route_missing(/.*/, :every_message)
+class TestMissingMethodRouter
+  include MethodMissingRouter
+end
 
-        def self.every_message
-        end
-      end
-      TestMissingMethodRouter.should_receive(:every_message)
-      TestMissingMethodRouter.blammo
+describe MethodMissingRouter do
+  let(:test_class) { TestMissingMethodRouter }
+
+  after(:each) do
+    test_class.reset_method_missing_routes!
+  end
+
+  context 'class method routing' do
+    it 'can route any missing message to the same target' do
+      test_class.class_route_missing(/.+/, :every_message)
+      test_class.should_receive(:every_message).twice
+      test_class.blammo
+      test_class.dragon_soda
+    end
+
+    it 'can route two different formats of message to different targets' do
+      test_class.class_route_missing(/^fish/, :fish_message)
+      test_class.class_route_missing(/[a-z]+/, :lowercase_message)
+
+      test_class.should_receive(:fish_message)
+      test_class.should_receive(:lowercase_message)
+
+      test_class.fish_powers_activate
+      test_class.lowercase
+    end
+
+    it 'passes undeclared messages to super' do
+      expect{test_class.something_unexpected}.to raise_error(NoMethodError)
     end
   end
 
   context 'instance method routing' do
-    example 'route any missing message to the same target' do
-      class TestMissingMethodRouter
-        include MethodMissingRouter
-        route_missing(/.*/, :every_message)
+    let(:test_instance) { test_class.new }
 
-        def every_message
-        end
-      end
-      test = TestMissingMethodRouter.new
-      test.should_receive(:every_message)
-      test.blammo
+    it 'can route any missing message to the same target' do
+      test_class.route_missing(/.*/, :every_message)
+      test_instance.should_receive(:every_message).twice
+
+      test_instance.blammo
+      test_instance.dragon_soda
+    end
+
+    it 'can route two different formats of message to different targets' do
+      test_class.route_missing(/^cow/, :cow_message)
+      test_class.route_missing(/in_a_bag$/, :in_a_bag)
+
+      test_instance.should_receive(:cow_message)
+      test_instance.should_receive(:in_a_bag)
+
+      test_instance.cowsplode
+      test_instance.pudding_in_a_bag
+    end
+
+    it 'passes undeclared messages to super' do
+      expect{test_instance.something_unexpected}.to raise_error(NoMethodError)
     end
   end
 end
