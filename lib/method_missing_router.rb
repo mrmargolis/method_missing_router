58a8bcc3de29148edbe12e6e9c32b9b55bb0ee3a
Matt Margolis <matt@mattmargolis.net>
2011-11-21 19:30:06 -0600

    Refactor so that class and instance code for method_missing is unified

--- lib/method_missing_router.rb
+++ lib/method_missing_router.rb
@@ -1,17 +1,31 @@
 require "method_missing_router/version"
 module MethodMissingRouter
   def self.included(base)
+    base.extend(Routing)
+    base.send(:include, Routing)
     base.extend(ClassMethods)
     base.reset_method_missing_routes!
   end
 
-  def method_missing(meth, *args, &block)
-    self.class.instance_variable_get("@method_missing_routes").each do |matcher, target|
-      args.unshift(meth)
-      return self.send(target, *args, &block) if matcher =~ meth
+
+  module Routing
+    def method_missing_routes
+      if self.kind_of?(Class)
+        @class_method_missing_routes
+      else 
+        self.class.instance_variable_get("@method_missing_routes")
+      end
     end
-    super(meth, *args, &block)
+
+    def method_missing(meth, *args, &block)
+      method_missing_routes.each do |matcher, target|
+        args.unshift(meth)
+        return self.send(target, *args, &block) if matcher =~ meth
+      end
+      super(meth, *args, &block)
+    end
   end
+
 
   module ClassMethods
     def class_route_missing(matcher, target)
@@ -25,14 +39,6 @@ module MethodMissingRouter
     def reset_method_missing_routes!
       @method_missing_routes = []
       @class_method_missing_routes = []
-    end
-
-    def method_missing(meth, *args, &block)
-      @class_method_missing_routes.each do |matcher, target|
-        args.unshift(meth)
-        return self.send(target, *args, &block) if matcher =~ meth
-      end
-      super(meth, *args, &block)
     end
   end
 end
